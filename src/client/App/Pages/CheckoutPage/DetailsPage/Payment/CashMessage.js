b64f1fb135602f3a3287cd6afd08f6fb4f69b9de
Osama <osmuab1997@gmail.com>
2020-04-19 07:05:12 -0500

    Checkout styling changes

--- src/client/App/Pages/CheckoutPage/DetailsPage/Payment/CashMessage.js
+++ src/client/App/Pages/CheckoutPage/DetailsPage/Payment/CashMessage.js
@@ -4,12 +4,14 @@ import styles from '../../CheckoutPage.css';
 
 const CashMessage = ({ saleMethod }) => {
     return (
-        <div className={ styles.CashMessage }>
+        <div className={ styles.CashMessageForm }>
+            <div className={ styles.CashMessageBody }>
             { saleMethod === 'delivery' ? (
                <span>Have your cash ready for the driver.</span> 
             ) : (
                 <span>You'll pay at the store.</span> 
             )}
+            </div>
         </div>
     )
 };
